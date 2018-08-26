b8548d237c5b34c39a20c289afc14d907767172d
William Melody <hi@williammelody.com>
2018-08-25 17:23:23 -0700

    Add test/test_helper.bash
    test_helper.bash defines global functions for bats tests. Use the `load`
    command at the top of each test file to source `test_helper`:
    
        #!/usr/bin/env bats
        load test_helper

---
+++ test/test_helper.bash
@@ -0,0 +1,34 @@
+###############################################################################
+# test_helper.bash
+#
+# Test helper for Bats: Bash Automated Testing System.
+#
+# https://github.com/sstephenson/bats
+###############################################################################
+
+setup() {
+  # `$_AIRPORT`
+  #
+  # The location of the `pb` script being tested.
+  export _AIRPORT="${BATS_TEST_DIRNAME}/../airport"
+}
+
+###############################################################################
+# Helpers
+###############################################################################
+
+# _compare()
+#
+# Usage:
+#   _compare <expected> <actual>
+#
+# Description:
+#   Compare the content of a variable against an expected value. When used
+#   within a `@test` block the output is only displayed when the test fails.
+_compare() {
+  local _expected="${1:-}"
+  local _actual="${2:-}"
+  printf "expected:\\n%s\\n" "${_expected}"
+  printf "actual:\\n%s\\n" "${_actual}"
+}
+
